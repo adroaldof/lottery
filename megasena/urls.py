13e0b94af74a7eba39e2b0ea5ede77f014df9dcb
Adroaldo de Andrade <adroaldof@gmail.com>
2013-10-14 23:18:29 -0300

    Add concourse details and its dependencies

--- megasena/urls.py
+++ megasena/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('megasena.views',
     url(r'^$', 'home', name='megasena-home'),
+    url(r'^detail/(?P<number>\d+)/$', 'detail', name='megasena-detail'),
 )
--- megasena/views.py
+++ megasena/views.py
@@ -8,3 +8,15 @@ def home(request):
     return TemplateResponse(request, 'megasena/home.html', {
         'raffles': raffles,
     })
+
+
+def detail(request, number):
+    last = Raffle.objects.exclude(n01__isnull=True).aggregate(Max('number'))['number__max']
+    if int(number) <= last:
+        infos = get_object_or_404(Raffle, number=number)
+        return TemplateResponse(request, 'megasena/detail.html', {
+            'infos': infos,
+        })
+    else:
+        messages.add_message(request, messages.INFO, _('This concourse was not raffled yet'))
+        return HttpResponseRedirect('/megasena')
