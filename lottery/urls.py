09c730fbe3b6ab085cb9019f46c4c87cdcb2c33b
Adroaldo de Andrade <adroaldof@gmail.com>
2013-10-16 13:10:01 -0300

    Apply pep8 to lottery

--- lottery/urls.py
+++ lottery/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', 'lottery.views.main_page', name='index'),
     url(r'^megasena/', include('megasena.urls')),
     url(r'^parse/', include('parsers.urls')),
