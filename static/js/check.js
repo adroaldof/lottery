f7bd94a7b47f54bc1a2d1fd3b37c3ba6cfe9c623
Adroaldo de Andrade <adroaldof@gmail.com>
2013-10-28 16:20:33 -0200

    Fix script on check page to display stubborns hits

--- static/js/check.js
+++ static/js/check.js
@@ -1,27 +1,28 @@
 $(function() {
-  var raffled = $('.raffled'),
-      all = $('.bet');
+	var table = $('table');
 
-  var concourses = new Array();
 
-  raffled.each(function() {
-    var concourse = parseInt($(this).text());
-    concourses.push(concourse);
-  });
+	table.each(function() {
+		var concourses = [];
 
-  all.each(function() {
-    var bet = parseInt($(this).text());
-    for (var i = 0; i < concourses.length; i++) {
-      if (bet === concourses[i]) {
-        $(this).css({'color': '#1632C2'}).parent().addClass('found-same-concourse');
-        
-      }
-    }
-  });
+		$(this).find('.raffled').each(function() {
+			var concourse = parseInt($(this).text());
+			concourses.push(concourse);
+		});
 
-  $('.bets').each(function() {
-    if ($(this).find('.found-same-concourse').length > 3) {
-      $(this).find('.check-status').addClass('match').removeClass('dont-match');
-    }
-  });
+		$(this).find('.bet').each(function() {
+			var bet = parseInt($(this).text());
+			for (var i = 0; i < concourses.length; i++) {
+				if (bet === concourses[i]) {
+					$(this).css({'color': '#1632C2'}).parent().addClass('found-same-concourse');
+				}
+			}
+		});
+
+		$(this).find('.bets').each(function() {
+			if ($(this).find('.found-same-concourse').length > 3) {
+				$(this).find('.check-status').addClass('match').removeClass('dont-match');
+			}
+		});
+	});
 });
